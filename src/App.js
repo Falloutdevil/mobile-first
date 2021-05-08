13f0cb1ec1189bc7e637f9a86e42651c53e28f01
Falloutdevil <Falloutdevil@ya.ru>
2021-05-08 15:53:03 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import s from './App.modue.scss';
+import Header from './components/blocks/header/Header';
+
+function App() {
+  return (
+    <div className={s.App}>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
